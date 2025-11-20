a0ede9093e627b21ffa62c086d016423e94e214e
tee-koo <72820951+tee-koo@users.noreply.github.com>
2025-11-20 13:32:25 +0200

    Update variables.tf

--- Zero Trust Network Architecture/terraform/modules/spoke/variables.tf
+++ Zero Trust Network Architecture/terraform/modules/spoke/variables.tf
@@ -1,6 +1,3 @@
 variable "resource_group_name" { type = string }
-variable "location"            { type = string }
-variable "hub_vnet_id"         { type = string }
-variable "firewall_private_ip" { type = string }
-variable "spoke_name"          { type = string }
-variable "spoke_cidr"          { type = string }
+variable "location" { type = string }
+variable "tags" { type = map(string) }
