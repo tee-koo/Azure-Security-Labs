5c1569175b9ac9c94247af07c22a974556968ac9
tee-koo <72820951+tee-koo@users.noreply.github.com>
2025-11-20 13:31:12 +0200

    Update outputs.tf

--- Zero Trust Network Architecture/terraform/modules/hub/outputs.tf
+++ Zero Trust Network Architecture/terraform/modules/hub/outputs.tf
@@ -1,5 +1,19 @@
-output "vnet_id"            { value = azurerm_virtual_network.hub.id }
-output "vnet_name"          { value = azurerm_virtual_network.hub.name }
-output "firewall_private_ip" { 
-  value = azurerm_firewall.central.ip_configuration[0].private_ip_address 
+output "vnet_name" {
+  value = azurerm_virtual_network.hub.name
+}
+
+output "vnet_id" {
+  value = azurerm_virtual_network.hub.id
+}
+
+output "firewall_private_ip" {
+  value = azurerm_firewall.hub_fw.ip_configuration[0].private_ip_address
+}
+
+output "firewall_public_ip" {
+  value = azurerm_public_ip.firewall_pip.ip_address
+}
+
+output "bastion_name" {
+  value = azurerm_bastion_host.hub_bastion.name
 }
