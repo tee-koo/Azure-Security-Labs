1ccc44f02b4d4db5b7f339b288b37aeea96b6aa5
tee-koo <72820951+tee-koo@users.noreply.github.com>
2025-11-20 13:34:22 +0200

    Update variables.tf

--- Zero Trust Network Architecture/terraform/variables.tf
+++ Zero Trust Network Architecture/terraform/variables.tf
@@ -1,5 +1,21 @@
+variable "resource_group_name" {
+  description = "Name of the shared resource group"
+  type        = string
+  default     = "rg-hub-spoke-v1"
+}
+
 variable "location" {
   description = "Azure region"
   type        = string
-  default     = "swedencentral"
+  default     = "Sweden Central"
+}
+
+variable "tags" {
+  description = "Common tags"
+  type        = map(string)
+  default = {
+    owner   = "tko"
+    user    = "tko"
+    expiry  = "2026-12-31"
+  }
 }
