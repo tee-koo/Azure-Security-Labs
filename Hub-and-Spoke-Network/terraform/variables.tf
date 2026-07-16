fff8f5a2000f4c9d128c9256d4a97673ce161bda
tee-koo <72820951+tee-koo@users.noreply.github.com>
2026-07-16 16:26:19 +0300

    Update variables.tf

--- Hub-and-Spoke-Network/terraform/variables.tf
+++ Hub-and-Spoke-Network/terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "resource_group_name" {
   description = "Name of the shared resource group"
   type        = string
-  default     = "rg-hub-spoke-v1"
+  default     = "rg-hub-spoke"
 }
 
 variable "location" {
